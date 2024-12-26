8b44695f17f8c99ca7e639faaf1264d88f759a76
Selman Kaya <selmankaya3412@gmail.com>
2024-12-26 19:57:58 +0300

    Initial Commit

---
+++ FotografPaylasma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FotografPaylasma
+//
+//  Created by Selman Kaya on 26.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FotografPaylasmaTests/FotografPaylasmaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FotografPaylasmaTests.swift
+//  FotografPaylasmaTests
+//
+//  Created by Selman Kaya on 26.12.2024.
+//
+
+import Testing
+@testable import FotografPaylasma
+
+struct FotografPaylasmaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
